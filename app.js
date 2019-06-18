135e2c0905fca3788308820e55e30bf0c7ea277e
kishoo <ahmed.elkashlan@student.guc.edu.eg>
2019-06-18 02:31:41 +0200

    add styling

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app=express();
 var items=["lunch","go to gym","sleep"];
 
 app.use(bodyParser.json(),bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
 
